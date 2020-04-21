cac01436b51720e67f2c582f3c3f1d81c7d70017
mona95 <shamona9594@gmail.com>
2020-04-21 08:54:27 +0300

    modify arrayBased stack and added top prop

--- DataStructures/Stack/Stack.js
+++ DataStructures/Stack/Stack.js
@@ -3,29 +3,44 @@
  *  The addition of new items or the removal of existing items takes place at the same end.
  *  The end of the stack is known as the top, and the opposite side is known as the base.
  *  The newest elements are near the top, and the oldest elements are near the base.
+ *  we are only able to execute the actions for the item at top of the stack (ILFO)
  */
 
 //Array-Based Stack
 export class StackArray {
   constructor() {
     this.items = [];
+    this.top = 0; //points to the top element index
   }
   push(element) {
-    this.items.push(element);
+    this.items[this.top] = element;
+    this.top++;
   }
   pop() {
-    this.items.pop();
+    if (!this.isEmpty()) {
+      this.top--;
+      return this.items.pop();
+    } else {
+      return `Stack is Empty,You can not remove`;
+    }
   }
   peek() {
-    return this.items[this.items.length - 1];
+    return this.items[this.top - 1];
   }
   isEmpty() {
-    return this.items.length === 0;
+    return this.top === 0;
   }
   size() {
-    return this.items.length;
+    return this.top;
   }
   clear() {
     this.items = [];
+  }
+  print() {
+    let top = this.top - 1; // because top points to index where new element to be inserted
+    while (top >= 0) {
+      console.log(this.items[top]);
+      top--;
+    }
   }
 }
