f0f2a64ba92456efd89fd8e461719e8e66402cec
mona95 <shamona9594@gmail.com>
2020-05-11 19:12:59 +0300

    update README.md

---
+++ DataStructures/Tree/BinarySearchTree.js
@@ -0,0 +1,5 @@
+/**
+ * A node in a `binary tree` has two children at most (this is really important) : one left child and one right child.
+ * a binary search tree (BST) is a binary tree , but it only allows you to store nodes with lesser values on left side
+ * and nodes with greater values on the right side.
+ */
