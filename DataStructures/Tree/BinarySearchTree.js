a13f9cf61ef8bf26f9487492e2f5dc9244dc5cd5
mona95 <shamona9594@gmail.com>
2020-05-12 20:55:30 +0300

    implement serach method

--- DataStructures/Tree/BinarySearchTree.js
+++ DataStructures/Tree/BinarySearchTree.js
@@ -53,7 +53,21 @@ export class BinarySearchTree {
       }
     }
   }
-  search(key) {}
+  search(key) {
+    return this.searchNode(this.root, key);
+  }
+  searchNode(node, key) {
+    if (node == null) {
+      return false;
+    }
+    if (this.compareFn(key, node.key) === compare.LESS_THAN) {
+      return this.searchNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === compare.BIGGER_THAN) {
+      return this.searchNode(node.right, key);
+    } else {
+      return true;
+    }
+  }
   /**
    * method visits all nodes of the tree using in-order travers
    * An in-order traversal visits all the nodes of a BST in an ascending order, meaning it will visit the nodes
