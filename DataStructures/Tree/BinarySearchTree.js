b52490f8587a243adc1309c13804f1e0a891f9a1
mona95 <shamona9594@gmail.com>
2020-05-13 19:56:07 +0300

    remove method for BST and guide comments

--- DataStructures/Tree/BinarySearchTree.js
+++ DataStructures/Tree/BinarySearchTree.js
@@ -139,5 +139,56 @@ export class BinarySearchTree {
     }
     return current;
   }
-  remove(key) {}
+  remove(key) {
+    //the root should receive the return of the removeNode method.
+    //because when u remove a node (depends on its position in tree) the depth level for the BST will change and the pointers will change
+    //so we need to assign the new structured tree to our root, there should be a reference always,
+    this.root = this.removeNode(this.root, key);
+  }
+  removeNode(node, key) {
+    if (node == null) {
+      //as a stopping point
+      return null;
+    }
+    // if root is not null, then we need to check the subtrees to find the key
+    // if the key we are looking for has the value lesser than current node, we go to the next node at the left-hand side
+    if (this.compareFn(key, node.key) === compare.LESS_THAN) {
+      node.left = this.removeNode(node.left, key); //updating the reference of the left pointer of the node
+      return node;
+      // if the key we are looking for has the value bigger than current node, we go to the next node at the right-hand side
+    } else if (this.compareFn(key, node.key) === compare.BIGGER_THAN) {
+      node.right = this.removeNode(node.right, key); //updating the reference of the right pointer of the node
+      return node;
+    } else {
+      //key is equal to node.key, so in this step we found the key, we have three scenarios to handle
+      //case 1 : `leaf node` means there is no left or right child for the node, so the node we are trying to remove is a leaf.
+      if (node.left == null && node.right == null) {
+        node = null;
+        //by assigning null to the node, the node is gonna be removed but thats not enough, we still have a pointer ,
+        //we need to assign null to its parent node, and this can be done by returning null,
+        return node;
+      }
+      //case 2 : if the node has left child or right child.in this case we need to skip the node that assign the parent pointer to the child node of current node,
+      if (node.left == null) {
+        //if the node doesn't have the left child, means it has right child so the parent should be assigned to right child.
+        node = node.right;
+        return node;
+      } else if (node.right == null) {
+        //if the node doesn't have the right child, means it has left child so the parent should be assigned to left child.
+        node = node.left;
+        return node;
+      }
+      //case 3: if the node has two children, in this case we need to follow 4 steps,as follows:
+      // 1-we need to find the minimun node from its right-hand side subtree.
+      // 2-then we will update the value of the node with the key of the minimun node from its right-hand side subtree, with this action, we are replacing
+      // the key of the node, which means it was removed.
+      // 3-now we have two nodes in the tree with same key, and this shouldn't happen.now we need to remove the minimum node from the right subtree since
+      //we moved it to the place of the removed node.
+      // 4- finally, we will return the updated node reference to its parent.
+      const aux = this.minNode(node.right);
+      node.key = aux.key;
+      node.right = this.removeNode(node.right, aux.key);
+      return node;
+    }
+  }
 }
