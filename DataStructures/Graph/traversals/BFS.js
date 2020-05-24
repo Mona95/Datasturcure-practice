87f9aa43544d6f08f00d537c841e9e704a9cfc6c
mona95 <shamona9594@gmail.com>
2020-05-24 19:19:38 +0300

    BFS implementation

--- DataStructures/Graph/traversals/BFS.js
+++ DataStructures/Graph/traversals/BFS.js
@@ -1,5 +1,53 @@
+import { initializeColor, Colors } from "./colors";
+import { Queue } from "../../../all";
+
 /**
  * Breadth-first search (BFS)
  * data structure for this algorithm is Queue , by storing the vertices in a queue, the oldest
  * unexplored vertices are explored first.
+ * the BFS algorithm starts traversing the graph from the first specified vertex and visits all its neighbors first, one layer of graph at a time.
+ * {@link https://learning.oreilly.com/library/view/learning-javascript-data/9781788623872/assets/a5e417b0-6ee9-470c-a697-583a95458260.png}
+ *
+ * these are the steps followed by the BFS algorithm, starting at vertex v :
+ * 1-Create a queue Q
+ * 2- mark v as discovered(grey) and enqueue v into Q
+ * 3- while Q is not empty, perform the following steps:
+ *      1- dequeue u from Q
+ *      2- mark u as discovered(grey)
+ *      3- enqueue all the unvisited(white) neighbors w of u
+ *      4- mark u as explored (black)
  */
+
+export const breadthFirstSearch = (graph, startVertex, callback) => {
+  const vertices = graph.getVertices(),
+    adjList = graph.getAdjList(),
+    color = initializeColor(vertices), //initializing the the color array with the white color
+    queue = new Queue(); // creating queue to store the vertices that need to be explored and visited.
+
+  queue.enqueue(startVertex); //we need a starting point so we will enqueue this vertex into the queue
+
+  while (!queue.isEmpty()) {
+    //as long as the queue is not empty, we will remove a vertex from the queue by using dequeue
+    //and we will get its adjacency list that conatins all its neighbors,also make the color GREY which means
+    //we have discovered the vertex but have not finished exploring it yet.
+    const u = queue.dequeue(),
+      neighbors = adjList.get(u);
+    color[u] = Colors.GREY;
+
+    //for each neighbor of u, we will obtain its value(the name of the vertex) and if it has not been visited yet(color is WHITE),
+    //we will mark that we have discovered it(color set to GREY),and adding this vertex to the queue so it can be finished exploring when we dequeue it
+    for (let i = 0; i < neighbors.length; i++) {
+      const w = neighbors[i];
+      if (color[w] === Colors.WHITE) {
+        color[w] = Colors.GREY;
+        queue.enqueue(w);
+      }
+    }
+
+    //when we are done with the vertex and its adjacent neighbors,we will set the color to BLACK
+    color[u] = Colors.BLACK;
+    if (callback) {
+      callback(u);
+    }
+  }
+};
